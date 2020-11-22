a395dd901f5f791675df0b4380f87a0dddd0f566
何伟芳 <heweifang0612@163.com>
2020-11-22 22:52:17 +0800

    集成reselect

---
+++ doc/reselect.md
@@ -0,0 +1,6 @@
+# reselect
+[https://github.com/reduxjs/reselect](https://github.com/reduxjs/reselect)
+
+- selector 中text变化，会引起getVisibleTodos的重新计算
+- reselect就是为了减少state的重新计算
+- state计算太负责的时候才使用
--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,17 +1,36 @@
+import { createSelector } from 'reselect'
 import { VISIBILITY_FILTERS } from '../store/action'
 
 export const getText = state => state.get('text')
 export const getFilter = state => state.get('filter')
-export const getVisibleTodos = state => {
-  const data = state.getIn(['todos', 'data'])
-  const filter = state.get('filter')
-  return data.filter(item => {
-    const completed = item.get('completed')
-    return filter === VISIBILITY_FILTERS.ACTIVE
-      ? !completed
-      : filter === VISIBILITY_FILTERS.COMPLETED
-        ? completed
-        : true
+
+const getTodos = state => state.getIn(['todos', 'data'])
+export const getVisibleTodos = createSelector(
+  [getTodos, getFilter],
+  (todos, filter) => {
+    console.log('getVisibleTodos')
+    return todos.filter(item => {
+      const completed = item.get('completed')
+      return filter === VISIBILITY_FILTERS.ACTIVE
+        ? !completed
+        : filter === VISIBILITY_FILTERS.COMPLETED
+          ? completed
+          : true
+    })
   }
-  )
-}
+)
+
+// export const getVisibleTodos = state => {
+//   const data = state.getIn(['todos', 'data'])
+//   const filter = state.get('filter')
+//   console.log('getVisibleTodos')
+//   return data.filter(item => {
+//       const completed = item.get('completed')
+//       return filter === VISIBILITY_FILTERS.ACTIVE
+//         ? !completed
+//         : filter === VISIBILITY_FILTERS.COMPLETED
+//           ? completed
+//           : true
+//     }
+//   )
+// }
