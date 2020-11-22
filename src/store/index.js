5f9fdb9442716fd73abdeb0889d79ca998c7e7f4
何伟芳 <heweifang0612@163.com>
2020-11-22 20:58:07 +0800

    store引用中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import todoApp from './reducer'
+import loggerMiddleware from '../middlewares/logger'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(todoApp, composeEnhancers(applyMiddleware(thunkMiddleware)))
+const store = createStore(
+  todoApp,
+  composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware,
+      loggerMiddleware
+    )
+  )
+)
 
 export default store
