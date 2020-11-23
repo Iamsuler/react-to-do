cc2452c43a40f9c67813eabcc3dbfb085dec4c4a
何伟芳 <heweifang0612@163.com>
2020-11-23 22:00:15 +0800

    定义路由

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Todo from './pages/Todo'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import User from './pages/User'
+import Nav from './components/Nav'
 
 class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <Router>
         <div>
-          <Route path="/" component={ Home }></Route>
-          <Route path="/todo" component={ Todo }></Route>
+          <Nav />
+          {/* switch 渲染第一个匹配的组件 */}
+          <Switch>
+            {/* exact保证是精准匹配 */}
+            {/* <Route path="/" exact component={ Home }></Route> */}
+            <Route path="/todo" component={ Todo }></Route>
+            <Route path="/about" component={ About }></Route>
+            <Route path="/contact" component={ Contact }></Route>
+            <Route path="/user/:id" component={ User }></Route>
+            <Route path="/" component={ Home }></Route>
+          </Switch>
         </div>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
